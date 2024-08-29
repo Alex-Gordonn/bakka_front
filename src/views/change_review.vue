f589ec10ec84163f2ce7d2f81016352012f928fd
Asylmurat <73055112+Asylmurat@users.noreply.github.com>
2024-08-29 10:42:15 +0500

    update review

--- src/views/change_review.vue
+++ src/views/change_review.vue
@@ -1,131 +1,161 @@
 <template>
-    <div class="sidebar" id="sidebar">
-      <router-view />
-      <div class="sidebar-header logo_admin">
-        <img src="../Bakka_new_logo/bakka_logo_blue.svg" alt="" />
-      </div>
-      <ul class="sidebar-menu">
-            <li @click="" class="sidebar-menu-item"><a ><img src="../img/icon_airplane.svg" alt=""> Туры</a></li>
-            <li @click="" class="sidebar-menu-item"><a ><img src="../img/Icon_guids.svg" alt=""> Гиды</a></li>
-            <li @click="goToReviews" class="sidebar-menu-item choosed"><a ><img src="../img/icon_review.svg" alt=""> Отзывы</a></li>
-            <li @click="" class="sidebar-menu-item"><a ><img src="../img/icon_news.svg" alt=""> Новости</a></li>
-            <li @click="" class="sidebar-menu-item"><a ><img src="../img/icon_qa.svg" alt=""> Вопросы и ответы</a></li>
-            <li @click="" class="sidebar-menu-item"><a ><img src="../img/icon_request.svg" alt=""> Список заявок</a></li>
-            <li @click="logout" class="sidebar-menu-item"><a><img src="../img/Icon_logout.svg" alt=""> Выйти</a></li>
-          </ul>
+  <div class="sidebar" id="sidebar">
+    <router-view />
+    <div class="sidebar-header logo_admin">
+      <img src="../Bakka_new_logo/bakka_logo_blue.svg" alt="" />
     </div>
-  
-    <div class="list_reviews">
-      <div class="row">
-        <div class="col text_review">
-          <h1>Отзывы</h1>
-        </div>
+    <ul class="sidebar-menu">
+      <li @click="" class="sidebar-menu-item"><a ><img src="../img/icon_airplane.svg" alt=""> Туры</a></li>
+      <li @click="" class="sidebar-menu-item"><a ><img src="../img/Icon_guids.svg" alt=""> Гиды</a></li>
+      <li @click="goToReviews" class="sidebar-menu-item choosed"><a ><img src="../img/icon_review.svg" alt=""> Отзывы</a></li>
+      <li @click="" class="sidebar-menu-item"><a ><img src="../img/icon_news.svg" alt=""> Новости</a></li>
+      <li @click="" class="sidebar-menu-item"><a ><img src="../img/icon_qa.svg" alt=""> Вопросы и ответы</a></li>
+      <li @click="" class="sidebar-menu-item"><a ><img src="../img/icon_request.svg" alt=""> Список заявок</a></li>
+      <li @click="logout" class="sidebar-menu-item"><a><img src="../img/Icon_logout.svg" alt=""> Выйти</a></li>
+    </ul>
+  </div>
+
+  <div class="list_reviews">
+    <div class="row">
+      <div class="col text_review">
+        <h1>Отзывы</h1>
       </div>
-      <div class="add_review">
-        <div class="row">
-          <div class="col">
-            <label for="name">
-              Имя <br />
-              <input v-model="review.name" type="text" name="name" id="name" />
-            </label>
-            <br />
-            <label for="rating">
-              Рэйтинг <br />
-              <select v-model="review.rating" name="rating" id="rating">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-              </select>
-            </label>
-          </div>
-          <div class="col file">
-            <label for="file">Фото человека <br>
-            <input type="file" id="file" @change="handleFileUpload" />
-            </label>
-          </div>
-        </div>
-        <div class="content">
-          <label for="content">
-            Комментарий <br />
-            <textarea v-model="review.content" name="content" id="content"></textarea>
+    </div>
+    <div class="add_review">
+      <div class="row">
+        <div class="col">
+          <label for="name">
+            Имя <br />
+            <input v-model="review.name" type="text" name="name" id="name" />
+          </label>
+          <br />
+          <label for="rating">
+            Рэйтинг <br />
+            <select v-model="review.rating" name="rating" id="rating">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
           </label>
         </div>
-        <button @click="submitReview">Отправить отзыв</button>
+        <div class="col file">
+          <label for="file">Фото человека <br>
+          <input type="file" id="file" @change="handleFileUpload" />
+          </label>
+        </div>
       </div>
+      <div class="content">
+        <label for="content">
+          Комментарий <br />
+          <textarea v-model="review.content" name="content" id="content"></textarea>
+        </label>
+      </div>
+      <button @click="submitReview">Обновить отзыв</button>
     </div>
-  </template>
-  
-  <script>
-  import axios from './axios';
-  
-  export default {
-    data() {
-      return {
-        review: {
-          name: '',
-          rating: 1,
-          content: '',
-          file: null,
-        },
-      };
+  </div>
+</template>
+
+<script>
+import axios from './axios';
+
+export default {
+  data() {
+    return {
+      review: {
+        id: null,
+        name: '',
+        rating: 1,
+        content: '',
+        file: null,
+      },
+    };
+  },
+  created() {
+    // Получаем ID отзыва из параметров маршрута
+    const reviewId = this.$route.query.id;
+    if (reviewId) {
+      this.loadReview(reviewId);
+    }
+  },
+  methods: {
+    async loadReview(reviewId) {
+      try {
+        // GET запрос для получения данных отзыва по ID
+        const response = await axios.get(`https://bakka.kz/api/reviews/${reviewId}/`);
+        if (response.status === 200) {
+          const data = response.data;
+          // Заполняем данные формы
+          this.review.id = data.id;
+          this.review.name = data.name;
+          this.review.rating = data.rating;
+          this.review.content = data.content;
+          // Примечание: Файл изображения не загружается напрямую в input type="file", но его можно показать, если нужно.
+        }
+      } catch (error) {
+        console.error('Ошибка при загрузке отзыва:', error);
+        alert('Не удалось загрузить данные отзыва.');
+      }
     },
-    methods: {
-      handleFileUpload(event) {
-        const file = event.target.files[0];
-        if (file) {
-          this.review.file = file;
-        } else {
-          this.review.file = null; // Убедитесь, что файл правильно обрабатывается
-        }
-      },
-      async submitReview() {
-        try {
-          const formData = new FormData();
-          formData.append('name', this.review.name);
-          formData.append('rating', this.review.rating);
-          formData.append('content', this.review.content);
-  
-          if (this.review.file) {
-            formData.append('image', this.review.file); // Используйте 'image' или нужный ключ для файла
-          }
-  
-          const response = await axios.post('https://bakka.kz/api/reviews/', formData, {
-            headers: {
-              'Content-Type': 'multipart/form-data',
-            },
-          });
-  
-          if (response.status === 201) {
-            alert('Отзыв успешно отправлен!');
-            this.clearForm();
-          } else {
-            alert('Произошла ошибка при отправке отзыва.');
-          }
-        } catch (error) {
-          console.error('Ошибка при отправке отзыва:', error);
-          alert('Ошибка при отправке. Попробуйте еще раз.');
-        }
-      },
-      clearForm() {
-        this.review.name = '';
-        this.review.rating = 1;
-        this.review.content = '';
+    handleFileUpload(event) {
+      const file = event.target.files[0];
+      if (file) {
+        this.review.file = file;
+      } else {
         this.review.file = null;
-        document.querySelector('input[type="file"]').value = null;
-      },
-      logout() {
-        this.$root.logout();
-        localStorage.removeItem('authToken');
-        this.$router.push('/');
-      },
-      goToReviews() {
-        this.$router.push({ name: 'reviews' });
-      },
+      }
     },
-  };
-  </script>
+    async submitReview() {
+      try {
+        const formData = new FormData();
+        formData.append('name', this.review.name);
+        formData.append('rating', this.review.rating);
+        formData.append('content', this.review.content);
+
+        if (this.review.file) {
+          formData.append('image', this.review.file); // Используйте 'image' или нужный ключ для файла
+        }
+
+        // Выполняем PUT запрос для обновления отзыва
+        const response = await axios.put(`https://bakka.kz/api/reviews/${this.review.id}/`, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+
+        if (response.status === 200) {
+          alert('Отзыв успешно обновлен!');
+          this.$router.push({ name: 'reviews' });
+        } else {
+          alert('Произошла ошибка при обновлении отзыва.');
+        }
+      } catch (error) {
+        console.error('Ошибка при обновлении отзыва:', error);
+        alert('Ошибка при отправке. Попробуйте еще раз.');
+      }
+    },
+    clearForm() {
+      this.review.id = null;
+      this.review.name = '';
+      this.review.rating = 1;
+      this.review.content = '';
+      this.review.file = null;
+      document.querySelector('input[type="file"]').value = null;
+    },
+    logout() {
+      this.$root.logout();
+      localStorage.removeItem('authToken');
+      this.$router.push('/');
+    },
+    goToReviews() {
+      this.$router.push({ name: 'reviews' });
+    },
+  },
+};
+</script>
+
+
   
   
     <style>
@@ -252,8 +282,6 @@
   }
   .add_review select {
     border-radius: 5px;
-  }
-  .file {
   }
   .content {
     text-align: left;
